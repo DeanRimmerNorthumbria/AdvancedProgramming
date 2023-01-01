bf1eb2c97611d49151d6af7690d77c5d49bb21e7
DeanRimmer <dean.rimmer1313@gmail.com>
2023-01-01 20:11:31 +0000

    Original Project Code Commit + work in progrees venur data layer

---
+++ day-away-planner/Migrations/202301011916443_SeedInitialVenueData.cs
@@ -0,0 +1,17 @@
+﻿namespace day_away_planner.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    using day_away_planner.Models;
+    
+    public partial class SeedInitialVenueData : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ day-away-planner/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using day_away_planner.Models;
+
+namespace day_away_planner
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(String[] args)
+        {
+            using (var context = new MyDBEntities())
+            {
+
+                //var venueList = context.Venues.ToList<Venue>();
+                //Venue a = new Venue();
+                //a.VenueName = "Test";
+                //a.VenueCost = 1;
+                //a.VenueLocation = "animal3";
+                //Console.WriteLine(a.VenueLocation);
+                //context.Venues.Add(a);
+                //context.SaveChanges();
+
+            }
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
