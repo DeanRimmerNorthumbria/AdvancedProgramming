e1eb3cb657071b8a19bf5fe06f6ec31da1072e84
BhanuprakashNorthumbria <120469950+BhanuprakashNorthumbria@users.noreply.github.com>
2023-01-06 13:48:29 +0000

    added file from ignored source

---
+++ day-away-planner/Models/IVenue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_away_planner.Models
+{
+    interface IVenue
+    {
+        int VenueID { get; set; }
+        string VenueName { get; set;}
+        double VenueCost { get; set; }
+        string VenueExtras { get; set; }
+        string VenueLocation { get; set; }
+        int VenueCapacity { get; set; }
+    }
+}
