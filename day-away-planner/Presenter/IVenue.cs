6115e9dbe19fb1b06b0911702b48874ba93f283c
BhanuprakashNorthumbria <120469950+BhanuprakashNorthumbria@users.noreply.github.com>
2023-01-06 20:20:03 +0000

    Feature Venue_view interface

--- day-away-planner/Presenter/IVenue.cs
+++ day-away-planner/Presenter/IVenue.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace day_away_planner.Models
+namespace day_away_planner.Presenter
 {
     interface IVenue
     {
         int VenueID { get; set; }
-        string VenueName { get; set;}
+        string VenueName { get; set; }
         double VenueCost { get; set; }
         string VenueExtras { get; set; }
         string VenueLocation { get; set; }
